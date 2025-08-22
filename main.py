a53e7cd725260f8e25a64a00a07f059795b427ef
m0nic-1232 <abobakola9@gmail.com>
2025-08-22 15:16:10 +0300

    v1.1
    final version

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pygame import *
 from random import randint
+
 window = display.set_mode((700,500))
 icon = image.load('ping-pong.png')  # путь к твоей иконке (размер лучше 32x32)
 display.set_icon(icon)
@@ -29,8 +30,10 @@ class Player_2(GameSprite):
             self.rect.y -= self.speed
         if keys_pressed[K_DOWN] and self.rect.y < 400:
             self.rect.y += self.speed
-speed_x = 2
-speed_y = 2
+speed_x1 = 1
+speed_y2 = 1
+abs_speed_x = 4
+abs_speed_y = 4
 player_1 = Player_1('wall.png',20,250,20,100,5)
 player_2 = Player_2('wall2.png',660,250,20,100,5)
 ball = GameSprite('ball.png',340,250,30,30,2)
@@ -52,40 +55,75 @@ while game:
     
     if not finish:
         window.blit(background,(0,0))
-        window.blit(text_score,(0,0))
-        window.blit(text_score1,(600,0))
+        
         player_1.reset()
         player_1.update()
         player_2.reset()
         player_2.update()
         ball.reset()
+        if score_1 == 3 or score_2 == 3:
+            abs_speed_x = 6
+            abs_speed_y = 6
+        speed_x = speed_x1 * abs_speed_x
+        speed_y = speed_y2 * abs_speed_y
         ball.rect.x += speed_x
         ball.rect.y += speed_y    
+        
         if ball.rect.y > 470 or ball.rect.y < 0:
-            speed_y *= -1
+            speed_y2 *= -1
         if ball.rect.x > 670:
             score_1 += 1
             text_score = score.render('Счет: '+ str(score_1),1,(255,255,255))
             ball.rect.x = 340
             ball.rect.y = 250
-            speed_x *= -1
-            speed_y *= -1
+            speed_x1 *= -1
+            speed_y2 *= -1
 
         if ball.rect.x < 0:
             score_2 += 1
             text_score1 = score1.render('Счет: '+ str(score_2),1,(255,255,255))
             ball.rect.x = 340
             ball.rect.y = 250
-            speed_x *= -1
-            speed_y *= -1
+            speed_x1 *= -1
+            speed_y2 *= -1
 
         if sprite.collide_rect(ball,player_1):   
-            speed_x *= -1
+            speed_x1 *= -1
+            speed_x = speed_x1 * abs_speed_x
             ball.rect.x += 10 * speed_x
         if sprite.collide_rect(ball,player_2):   
-            speed_x *= -1
+            speed_x1 *= -1
+            speed_x = speed_x1 * abs_speed_x
             ball.rect.x += 10 * speed_x
 
+        
+        
+        window.blit(text_score,(0,0))
+        window.blit(text_score1,(600,0))
+        if score_1 == 10:
+            win_1 = font.SysFont("Arial",40)
+            text_win_1 = win_1.render('Победил 1 игрок! ',True,(0,255,0))
+            window.blit(text_win_1,(250,0))
+            finish = True
+        if score_2 == 10:
+            win_2 = font.SysFont('Arial', 40)
+            text_win_2 = win_2.render('Победил 2 игрок',True,(0,255,0))
+            window.blit(text_win_2,(250,0))
+            finish = True
+    else:
+        time.delay(3600)
+        finish = False
+        score_1 = 0
+        text_score = score.render('Счет: '+ str(score_1),1,(255,255,255))
+        score_2 = 0
+        text_score1 = score1.render('Счет: '+ str(score_2),1,(255,255,255))
+        ball.rect.x = 340
+        ball.rect.y = 250
+        speed_x1 = 1
+        speed_y2 = 1
+        abs_speed_x = 4
+        abs_speed_y = 4
+        
 
 
 
@@ -101,7 +139,5 @@ while game:
 
 
 
-
-
-        display.update()
-        clock.tick(FPS)
+    display.update()
+    clock.tick(FPS)
